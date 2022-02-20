4551e53a05f3911ee54ece9a0c37ead59cfebcc9
Clement Deloof <clement.deloof@Ugent.be>
2022-02-20 16:49:17 +0100

    added colors/logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>OSOC</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Open Summer Of Code</h1>
       </header>
     </div>
   );
