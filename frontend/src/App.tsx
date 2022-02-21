7d2e309dbf75163ca7cfd8758a606f33ff7d7671
Clement Deloof <clement.deloof@Ugent.be>
2022-02-21 03:52:55 +0100

    added and customized buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "./Buttons.css";
 
 function App() {
     return (
@@ -9,6 +10,18 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1>Open Summer Of Code</h1>
+                <button className="red_login_button">
+                    Mail account
+                    <span></span>
+                </button>
+                <button className="green_login_button">
+                    Google
+                    <span></span>
+                </button>
+                <button prefix="e-icon e-handup" className="red_login_button">
+                    Github
+                    <span></span>
+                </button>
             </header>
         </div>
     );
