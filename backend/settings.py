7778032d3a43cae55dba8075abf7f41df21cc63b
stijndcl <declercq.stijn@outlook.com>
2022-02-24 22:29:02 +0100

    Add default db password

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,7 @@ DB_NAME: str = env.str("DB_NAME", "osoc_dev")
 # Username used to log in
 DB_USERNAME: str = env.str("DB_USER", "root")
 # Password used to log in
-DB_PASSWORD: str = env.str("DB_PASSWORD")
+DB_PASSWORD: str = env.str("DB_PASSWORD", "password")
 # IP-address the database is running on
 DB_HOST: str = env.str("DB_HOST", "0.0.0.0")
 # Port the database is running on
