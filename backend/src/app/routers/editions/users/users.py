1c48fdac55084feb9034ec3b7b1c6ef28ea8c5ef
SeppeM8 <seppe.maes8@gmail.com>
2022-03-07 12:32:18 +0100

    get_user, begin test

--- backend/src/app/routers/editions/users/users.py
+++ backend/src/app/routers/editions/users/users.py
@@ -1,15 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 from src.app.routers.tags import Tags
+from src.database.database import get_session
+from src.database.models import User
 
 users_router = APIRouter(prefix="/users", tags=[Tags.USERS])
 
 
-@users_router.get("/")
-async def get_users(edition_id: int):
+class UserBase(BaseModel):
+    user_id: int
+    name: str
+    email: str
+
+
+@users_router.get("/", response_model=list[UserBase])
+async def get_users(edition_id: int, db: Session = Depends(get_session)) -> list[User]:
+    """Get a list of all users from given edition.
+    Args:
+        edition_id (int): the id of the edition of which the users need to be
+        db (Session, optional): connection with the database. Defaults to Depends(get_session).
+    Returns:
+        list[User]: a list of all users from the given edition
     """
-    Get a list of all users (admins & coaches).
-    """
+    return db.query(User).all()  # TODO: filter edition
 
 
 @users_router.patch("/{user_id}/status")
