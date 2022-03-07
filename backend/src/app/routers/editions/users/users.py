5a3d9fe341687cd265fa4b99c20c6cf9c1973203
SeppeM8 <seppe.maes8@gmail.com>
2022-03-07 12:32:18 +0100

    get_user, begin test

--- backend/src/app/routers/editions/users/users.py
+++ backend/src/app/routers/editions/users/users.py
@@ -1,47 +1,41 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from src.app.routers.tags import Tags
-import src.app.logic.users as logic
-from src.app.schemas.users import UsersListResponse, StatusBody, RequestAnswer
 from src.database.database import get_session
-
+from src.database.models import User
 
 users_router = APIRouter(prefix="/users", tags=[Tags.USERS])
 
 
-@users_router.get("/", response_model=UsersListResponse)
-async def get_users(edition_id: int, db: Session = Depends(get_session)):
+class UserBase(BaseModel):
+    user_id: int
+    name: str
+    email: str
+
+
+@users_router.get("/", response_model=list[UserBase])
+async def get_users(edition_id: int, db: Session = Depends(get_session)) -> list[User]:
+    """Get a list of all users from given edition.
+    Args:
+        edition_id (int): the id of the edition of which the users need to be
+        db (Session, optional): connection with the database. Defaults to Depends(get_session).
+    Returns:
+        list[User]: a list of all users from the given edition
     """
-    Get a list of all users from given edition.
+    return db.query(User).all()  # TODO: filter edition
+
+
+@users_router.patch("/{user_id}/status")
+async def update_user_status(edition_id: int, user_id: int):
+    """
+    Update the status of a user (admin/coach/disabled).
     """
 
-    return logic.get_users_list(db, edition_id)
 
-
-@users_router.post("/{user_id}/coach", status_code=204)
-async def add_user_as_coach(edition_id: int, user_id, int, db: Session = Depends(get_session)):
-    """
-    Add user as coach for the given edition
-    """
-
-    logic.add_user_as_coach(db, edition_id, user_id)
-
-
-@users_router.delete("/{user_id}/coach", status_code=204)
-async def delete_user_as_coach(edition_id: int, user_id, int, db: Session = Depends(get_session)):
-    """
-    Remove user as coach for the given edition
-    """
-
-    logic.delete_user_as_coach(db, edition_id, user_id)
-
-
-@users_router.post("/{user_id}/request", status_code=204)
-async def handle_user_request(edition_id: int, user_id: int, answer: RequestAnswer, db: Session = Depends(get_session)):
+@users_router.post("/{user_id}/request")
+async def handle_user_request(edition_id: int, user_id: int):
     """
     Accept or decline the pending invite request for a given user.
-    Accept will add the user as coach to the edition
     """
-
-    logic.handle_user_request(db, edition_id, user_id, answer)
