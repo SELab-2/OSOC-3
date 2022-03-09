1b14b1844e6d9951466d84234e66bc19dfb12fbf
beguille <bert.guillemyn@ugent.be>
2022-03-09 14:33:50 +0100

    changed token expire time to 1 day

--- backend/src/app/routers/login/login.py
+++ backend/src/app/routers/login/login.py
@@ -28,8 +28,7 @@ env.read_env()
 
 SECRET_KEY: str = env.str("SECRET_KEY")
 ALGORITHM = "HS256"
-# TODO: set time wanted
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_HOURS = 24
 
 fake_users_db = {
     123456789: {
@@ -110,8 +109,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        # TODO: set time wanted
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(hours=24)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -154,7 +152,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
     access_token = create_access_token(
         data={"sub": str(user.user_id)}, expires_delta=access_token_expires
     )
