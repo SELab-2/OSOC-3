2aaf5581df839d12c049590a2a0f4e7c78ee32d2
SeppeM8 <seppe.maes8@gmail.com>
2022-03-11 15:29:59 +0100

    get_users route (incl TODO for admins)

--- backend/src/database/crud/users.py
+++ backend/src/database/crud/users.py
@@ -1,15 +1,12 @@
-from sqlalchemy import select
-
 from src.database.models import User, UserRole
-from sqlalchemy.orm import Session, aliased
+from sqlalchemy.orm import Session
 
 
 def get_users_from_edition(db: Session, edition_id: int) -> list[User]:
     """Get all users from the given edition"""
 
-    a1 = aliased(UserRole)
-    coaches = db.execute(select(User).join(a1, User.user_id == UserRole.user_id).where(a1.edition_id == edition_id))
-    #admins = db.execute(select(User)) TODO: admins
+    coaches = db.query(User).join(UserRole).where(UserRole.edition_id == edition_id).all()
 
-    return coaches #+ admins
+    # TODO: admins (depends on changes in db)
 
+    return coaches
