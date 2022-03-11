d40d14e4f7dfd400a20a73b1aeb6f0d95eb31e2c
SeppeM8 <seppe.maes8@gmail.com>
2022-03-11 15:29:59 +0100

    get_users route (incl TODO for admins)

--- backend/tests/test_database/test_crud/test_users.py
+++ backend/tests/test_database/test_crud/test_users.py
@@ -6,15 +6,22 @@ from src.database.enums import RoleEnum
 
 
 def test_get_users_from_edition(database_session: Session):
-    pass
+
+    # Create users
     user1 = models.User(name="user1", email="email1")
     database_session.add(user1)
     user2 = models.User(name="user2", email="email2")
     database_session.add(user2)
+
+    # Create editions
     edition1 = models.Edition(year=1)
     database_session.add(edition1)
     edition2 = models.Edition(year=2)
     database_session.add(edition2)
+
+    database_session.commit()
+
+    # Create roles
     user1_edition1_role = models.UserRole(user_id=user1.user_id, role=RoleEnum.COACH, edition_id=edition1.edition_id)
     database_session.add(user1_edition1_role)
     user2_edition1_role = models.UserRole(user_id=user2.user_id, role=RoleEnum.COACH, edition_id=edition1.edition_id)
@@ -32,5 +39,4 @@ def test_get_users_from_edition(database_session: Session):
 
     users = get_users_from_edition(database_session, edition2.edition_id)
     assert len(users) == 1, "Wrong length"
-    assert user1.user_id == users[0].user_id
-
+    assert user2.user_id == users[0].user_id
