b77f913987d7ad6bf8ff5735a793d71434726d8b
Tiebe Vercoutter <tiebe.vercoutter@ugent.be>
2022-03-12 17:35:18 +0100

    added CamelCaseModel to existing schemas

--- backend/src/app/schemas/editions.py
+++ backend/src/app/schemas/editions.py
@@ -1,11 +1,27 @@
 from pydantic import BaseModel
+from humps import camelize
+
+
+def to_camel(string: str) -> str:
+    return camelize(string)
+
+
+class CamelCaseModel(BaseModel):
+    """ To have camel-case JSON request/response body while keeping the models snake-cased
+
+    Args:
+        BaseModel (BaseModel): needed to make the CamelCaseModel pydantic
+    """
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
 
 
 class EditionBase(BaseModel):
     """Schema of an edition
 
     Args:
-        BaseModel (BaseModel): needed to make EditionBase pydantic.
+        CamelCaseModel (CamelCaseModel): needed to make EditionBase pydantic.
     """
     year: int
 
--- backend/src/app/schemas/skills.py
+++ backend/src/app/schemas/skills.py
@@ -1,11 +1,27 @@
 from pydantic import BaseModel
+from humps import camelize
 
 
-class SkillBase(BaseModel):
+def to_camel(string: str) -> str:
+    return camelize(string)
+
+
+class CamelCaseModel(BaseModel):
+    """ To have camel-case JSON request/response body while keeping the models snake-cased
+
+    Args:
+        BaseModel (BaseModel): needed to make the CamelCaseModel pydantic
+    """
+    class Config:
+        alias_generator = to_camel
+        allow_population_by_field_name = True
+
+
+class SkillBase(CamelCaseModel):
     """Schema of a skill
 
     Args:
-        BaseModel (BaseModel): needed to make SkillBase pydantic.
+        CamelCaseModel (CamelCaseModel): needed to make SkillBase pydantic.
     """
     name: str
     desc: str
