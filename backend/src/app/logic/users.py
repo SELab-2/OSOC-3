5c3cf2be8fde55e758a3c817feb9f4059f1d2fef
SeppeM8 <seppe.maes8@gmail.com>
2022-03-12 15:08:37 +0100

    update user status

--- backend/src/app/logic/users.py
+++ backend/src/app/logic/users.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 
-from src.app.schemas.users import UsersListResponse
-from src.database.crud.users import get_users_from_edition
+from src.app.schemas.users import UsersListResponse, StatusBody
+import src.database.crud.users as users_crud
+from src.database.enums import RoleEnum
 
 
 def get_users_list(db: Session, edition: int) -> UsersListResponse:
@@ -9,7 +10,12 @@ def get_users_list(db: Session, edition: int) -> UsersListResponse:
     Query the database for a list of users
     and wrap the result in a pydantic model
     """
-    users_orm = get_users_from_edition(db, edition)
+    users_orm = users_crud.get_users_from_edition(db, edition)
     return UsersListResponse(users=users_orm)
 
 
+def update_user_status(db: Session, edition_id: int, user_id: int, status: StatusBody):
+    """Change the status of a given user in an edition"""
+    # Create db entry
+    status = {StatusBody.DISABLED: RoleEnum.DISABLED, StatusBody.ADMIN: RoleEnum.ADMIN, StatusBody.COACH: RoleEnum.COACH}[status]
+    users_crud.update_user_status(db, edition_id, user_id, status)
