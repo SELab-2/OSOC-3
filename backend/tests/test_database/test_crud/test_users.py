095466b2951e3383ecc9444f1c26ea3959da4470
SeppeM8 <seppe.maes8@gmail.com>
2022-03-12 13:20:56 +0100

    update user status

--- backend/tests/test_database/test_crud/test_users.py
+++ backend/tests/test_database/test_crud/test_users.py
@@ -1,12 +1,11 @@
 from sqlalchemy.orm import Session
 
 from src.database import models
-from src.database.crud.users import get_users_from_edition
+import src.database.crud.users as users_crud
 from src.database.enums import RoleEnum
 
 
 def test_get_users_from_edition(database_session: Session):
-
     # Create users
     user1 = models.User(name="user1", email="email1")
     database_session.add(user1)
@@ -31,12 +30,33 @@ def test_get_users_from_edition(database_session: Session):
 
     database_session.commit()
 
-    users = get_users_from_edition(database_session, edition1.edition_id)
+    users = users_crud.get_users_from_edition(database_session, edition1.edition_id)
     assert len(users) == 2, "Wrong length"
     user_ids = [user.user_id for user in users]
     assert user1.user_id in user_ids
     assert user2.user_id in user_ids
 
-    users = get_users_from_edition(database_session, edition2.edition_id)
+    users = users_crud.get_users_from_edition(database_session, edition2.edition_id)
     assert len(users) == 1, "Wrong length"
     assert user2.user_id == users[0].user_id
+
+
+def test_update_user_status(database_session: Session):
+    # Create users
+    user1 = models.User(name="user1", email="email1")
+    database_session.add(user1)
+
+    # Create editions
+    edition1 = models.Edition(year=1)
+    database_session.add(edition1)
+
+    database_session.commit()
+
+    # Create roles
+    user1_edition1_role = models.UserRole(user_id=user1.user_id, role=RoleEnum.COACH, edition_id=edition1.edition_id)
+    database_session.add(user1_edition1_role)
+
+    database_session.commit()
+
+    users_crud.update_user_status(database_session, edition1.edition_id, user1.user_id, RoleEnum.DISABLED)
+    assert user1_edition1_role.role == RoleEnum.DISABLED
--- backend/tests/test_routers/test_editions/test_users/test_users.py
+++ backend/tests/test_routers/test_editions/test_users/test_users.py
@@ -1,7 +1,32 @@
-from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
+from starlette.testclient import TestClient
+from starlette import status
+
+from src.database import models
+from src.database.enums import RoleEnum
 
 
-def test_get_users(test_client: TestClient):
-    response = test_client.get("/editions/123/users/")
-    print(response.status_code)
-    print(response.json())
+def test_get_users(database_session: Session, test_client: TestClient):
+    # Create users
+    user1 = models.User(name="user1", email="email1")
+    database_session.add(user1)
+
+    # Create editions
+    edition1 = models.Edition(year=1)
+    database_session.add(edition1)
+
+    database_session.commit()
+
+    # Create roles
+    user1_edition1_role = models.UserRole(user_id=user1.user_id, role=RoleEnum.COACH, edition_id=edition1.edition_id)
+    database_session.add(user1_edition1_role)
+
+    database_session.commit()
+
+    response = test_client.get(f"/editions/{edition1.edition_id}/users")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()
+
+
