8ea0f6367335708a9eb4d86c5ab8281738b1fa24
Clement Deloof <clement.deloof@Ugent.be>
2022-03-12 17:08:08 +0100

    centered everything and letters added

--- frontend/src/components/LogInButtons.tsx
+++ frontend/src/components/LogInButtons.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'
 import "../css-files/LogInButtons.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function LogInButtons() {
     const navigate = useNavigate();
@@ -10,26 +9,17 @@ function LogInButtons() {
             <div>
                 <div className="email-login">
                     <button onClick={() => navigate("/register")} className="login-button">
-                        <div className="login-button-content">
-                            <FontAwesomeIcon icon="envelope" className="mail-icon"/>
-                            <span>Log in with email</span>
-                        </div>
+                        <span>Log in with email</span>
                     </button>
                 </div>
                 <div className="google-login">
                     <button onClick={() => navigate("/register")} className="login-button">
-                        <div className="login-button-content">
-                            <FontAwesomeIcon icon="envelope" className="google-icon"/>
-                            <span>Log in with Google</span>
-                        </div>
+                        <span>Log in with Google</span>
                     </button>
                 </div>
                 <div className="github-login">
                     <button onClick={() => navigate("/register")} className="login-button">
-                        <div className="login-button-content">
-                            <FontAwesomeIcon icon="envelope" className="github-icon"/>
-                            <span>Log in with Github</span>
-                        </div>
+                        <span>Log in with Github</span>
                     </button>
                 </div>
             </div>
--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Nav, NavLink, Bars, NavMenu} from "./NavBarElementss";
 
+
 function NavBar() {
     return(
         <>
