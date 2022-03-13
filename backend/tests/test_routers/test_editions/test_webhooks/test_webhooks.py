beb1b2cec686336d7b9ca2ae644fe2daaa66420a
Francis <francisklinck@gmail.com>
2022-03-13 13:28:29 +0100

    happy flow test

--- backend/tests/test_routers/test_editions/test_webhooks/test_webhooks.py
+++ backend/tests/test_routers/test_editions/test_webhooks/test_webhooks.py
@@ -1,16 +1,28 @@
+import json
+
 import pytest
 from fastapi.testclient import TestClient
-from src.database.models import Edition
+from src.database.models import Edition, WebhookURL
+from sqlalchemy.orm import Session
 from starlette import status
 from uuid import UUID
+from .data import WEBHOOK_EVENT
 
 
 @pytest.fixture
-def edition(database_session) -> Edition:
+def edition(database_session: Session) -> Edition:
     edition = Edition(year=2022)
     database_session.add(edition)
     database_session.commit()
     return edition
+
+
+@pytest.fixture
+def webhook(edition: Edition, database_session: Session) -> WebhookURL:
+    webhook = WebhookURL(edition=edition)
+    database_session.add(webhook)
+    database_session.commit()
+    return webhook
 
 
 def test_new_webhook(test_client: TestClient, edition: Edition):
@@ -25,9 +37,9 @@ def test_new_webhook_invalid_edition(test_client: TestClient, edition: Edition):
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
-def test_webhook_trigger(test_client: TestClient, edition: Edition):
-    response = test_client.post(f"/editions/{edition.edition_id}/webhooks/")
+def test_webhook_trigger(test_client: TestClient, webhook: WebhookURL):
+    response = test_client.post(f"/editions/{webhook.edition_id}/webhooks/")
     assert response.status_code == status.HTTP_200_OK
     uuid = response.json()['uuid']
-    response = test_client.post(f"/editions/{edition.edition_id}/webhooks/{uuid}", data={})
-    assert response.status_code == status.HTTP_404_NOT_FOUND
+    response = test_client.post(f"/editions/{webhook.edition_id}/webhooks/{webhook.uuid}", json=WEBHOOK_EVENT)
+    assert response.status_code == status.HTTP_200_OK
