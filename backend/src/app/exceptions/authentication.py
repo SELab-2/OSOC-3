600be3efb69d35976dd76bea0e18bb3b3924437e
stijndcl <declercq.stijn@outlook.com>
2022-03-13 17:46:56 +0100

    Add docstring comments

--- backend/src/app/exceptions/authentication.py
+++ backend/src/app/exceptions/authentication.py
@@ -1,10 +1,23 @@
 class ExpiredCredentialsException(ValueError):
+    """
+    Exception raised when a user's token was valid, but has
+    expired in the meantime
+    A new token should be requested
+    """
     pass
 
 
 class InvalidCredentialsException(ValueError):
+    """
+    Exception raised when invalid username/password combination or
+    invalid bearer token are passed as authorization credentials
+    """
     pass
 
 
 class UnauthorizedException(ValueError):
+    """
+    Exception raised when a request to a private route is made without
+    a (valid) bearer token provided in the headers
+    """
     pass
