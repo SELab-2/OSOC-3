3d70960d75b69951a2b8a9c3ff69aaca1c5457fd
Francis <francisklinck@gmail.com>
2022-03-13 13:37:58 +0100

    add bad format test

--- backend/tests/test_routers/test_editions/test_webhooks/test_webhooks.py
+++ backend/tests/test_routers/test_editions/test_webhooks/test_webhooks.py
@@ -1,12 +1,12 @@
-import json
+from uuid import UUID
 
 import pytest
 from fastapi.testclient import TestClient
-from src.database.models import Edition, WebhookURL
 from sqlalchemy.orm import Session
 from starlette import status
-from uuid import UUID
-from .data import WEBHOOK_EVENT
+
+from src.database.models import Edition, WebhookURL
+from .data import WEBHOOK_EVENT, WEBHOOK_EVENT_BAD_FORMAT
 
 
 @pytest.fixture
@@ -37,9 +37,14 @@ def test_new_webhook_invalid_edition(test_client: TestClient, edition: Edition):
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
-def test_webhook_trigger(test_client: TestClient, webhook: WebhookURL):
-    response = test_client.post(f"/editions/{webhook.edition_id}/webhooks/")
-    assert response.status_code == status.HTTP_200_OK
-    uuid = response.json()['uuid']
+def test_webhook(test_client: TestClient, webhook: WebhookURL):
     response = test_client.post(f"/editions/{webhook.edition_id}/webhooks/{webhook.uuid}", json=WEBHOOK_EVENT)
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_webhook_bad_format(test_client: TestClient, webhook: WebhookURL):
+    response = test_client.post(
+        f"/editions/{webhook.edition_id}/webhooks/{webhook.uuid}",
+        json=WEBHOOK_EVENT_BAD_FORMAT
+    )
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
