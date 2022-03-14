f6d01fc11405a272fd5ba4c6b4c3a0c135a5a27a
Ward Meersman <wardmeersman@gmail.com>
2022-03-14 20:03:40 +0100

    creat user, emailauth and coachrequest

--- backend/src/database/crud/register.py
+++ backend/src/database/crud/register.py
@@ -1,9 +1,24 @@
 from sqlalchemy.orm import Session
 
-from src.database.models import User
+from src.database.models import AuthEmail, CoachRequest, User
+
+import hashlib
 
 def create_user(db: Session, name: str, email: str) -> User:
-    new_user: User = User(name="Jos", email="mail@email.com")
+    new_user: User = User(name=name, email=email)
     db.add(new_user)
     db.commit()
     return new_user
+
+def create_coach_request(db: Session, user: User) -> CoachRequest:
+    coach_request: CoachRequest = CoachRequest(user=user)
+    db.add(coach_request)
+    db.commit()
+    return coach_request
+
+def create_auth_email(db: Session, user: User, pw: str) -> AuthEmail:
+    pw_hash = hashlib.sha512(pw.encode("UTF-8")).hexdigest()
+    auth_email : AuthEmail = AuthEmail(user=user, pw_hash = pw_hash)
+    db.add(auth_email)
+    db.commit()
+    return auth_email
