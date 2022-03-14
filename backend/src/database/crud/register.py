8f308208e2f50382c6f6bc28b8ba94bb2c3982ab
Ward Meersman <wardmeersman@gmail.com>
2022-03-14 19:20:25 +0100

    start of register

--- backend/src/database/crud/register.py
+++ backend/src/database/crud/register.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from src.database.models import User
 
 def create_user(db: Session, name: str, email: str) -> User:
-    user = User(name="Jos", email="mail@email.com")
-    db.add(user)
+    new_user: User = User(name="Jos", email="mail@email.com")
+    db.add(new_user)
     db.commit()
-    return user
+    return new_user
