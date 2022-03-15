29c8d2dfa0edf182007f2ba91eb0e675c35d3876
Ward Meersman <wardmeersman@gmail.com>
2022-03-15 15:52:14 +0100

    added documentation

--- backend/src/app/logic/register.py
+++ backend/src/app/logic/register.py
@@ -5,6 +5,7 @@ from src.database.models import Edition
 from src.database.crud.register import create_user, create_auth_email, create_coach_request
 
 def create_request(db: Session, new_user: NewUser, edition: Edition) -> None:
+    """Create a coach request. But when something failed, the changed aren't commited"""
     try:
         user = create_user(db, new_user.name, new_user.email)
         create_auth_email(db, user, new_user.pw)
