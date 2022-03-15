917b344c2948ea768c7bb1233f022fe176cd2438
Ward Meersman <48222993+WardM99@users.noreply.github.com>
2022-03-15 13:28:35 +0100

    Update backend/tests/test_routers/test_editions/test_register/test_register.py
    Co-authored-by: Francis <44001949+FKD13@users.noreply.github.com>

--- backend/tests/test_routers/test_editions/test_register/test_register.py
+++ backend/tests/test_routers/test_editions/test_register/test_register.py
@@ -8,5 +8,5 @@ from src.database.models import Edition
 def test_ok(database_session: Session, test_client: TestClient):
     database_session.add(Edition(year=2022))
     database_session.commit()
-    response = test_client.post("/editions/1/register/email", data=dumps({"name": "Joskes vermeulen","email": "jw@gmail.com", "pw": "test"}))
+    response = test_client.post("/editions/1/register/email", json={"name": "Joskes vermeulen","email": "jw@gmail.com", "pw": "test"})
     assert response.status_code == status.HTTP_201_CREATED
