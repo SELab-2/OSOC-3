bb893ab7ac80ead0a3a62cc9ec0453a9c3c13f9a
Ward Meersman <wardmeersman@gmail.com>
2022-03-15 15:06:24 +0100

    coach request is linked to edition

--- backend/src/database/crud/register.py
+++ backend/src/database/crud/register.py
@@ -1,22 +1,25 @@
 from sqlalchemy.orm import Session
 
-from src.database.models import AuthEmail, CoachRequest, User
+from src.database.models import AuthEmail, CoachRequest, User, Edition
 
 import hashlib
 
 def create_user(db: Session, name: str, email: str) -> User:
+    """Create a user"""
     new_user: User = User(name=name, email=email)
     db.add(new_user)
     db.commit()
     return new_user
 
-def create_coach_request(db: Session, user: User) -> CoachRequest:
-    coach_request: CoachRequest = CoachRequest(user=user)
+def create_coach_request(db: Session, user: User, edition: Edition) -> CoachRequest:
+    """Create a coach request"""
+    coach_request: CoachRequest = CoachRequest(user=user, edition=edition)
     db.add(coach_request)
     db.commit()
     return coach_request
 
 def create_auth_email(db: Session, user: User, pw: str) -> AuthEmail:
+    """Create a authentication for email"""
     pw_hash = hashlib.sha512(pw.encode("UTF-8")).hexdigest()
     auth_email : AuthEmail = AuthEmail(user=user, pw_hash = pw_hash)
     db.add(auth_email)
--- backend/tests/test_database/test_crud/test_register.py
+++ backend/tests/test_database/test_crud/test_register.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from src.database.models import AuthEmail, CoachRequest, User
+from src.database.models import AuthEmail, CoachRequest, User, Edition
 
 from src.database.crud.register import create_user, create_coach_request, create_auth_email
 
@@ -13,9 +13,12 @@ def test_create_user(database_session: Session):
     assert a[0].email == "mail@email.com"
 
 def test_react_coach_request(database_session: Session):
+    edition = Edition(year = 2022)
+    database_session.add(edition)
+    database_session.commit()
     u = create_user(database_session, "jos", "mail@email.com")
-    create_coach_request(database_session, u)
-
+    
+    create_coach_request(database_session, u, edition)
     a = database_session.query(CoachRequest).where(CoachRequest.user == u).all()
 
     assert len(a) == 1
