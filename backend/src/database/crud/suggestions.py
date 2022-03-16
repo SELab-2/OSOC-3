7fccca115a95a334e32f908d4545bb7e602b7c88
Ward Meersman <wardmeersman@gmail.com>
2022-03-16 16:14:46 +0100

    start suggestions

--- backend/src/database/crud/suggestions.py
+++ backend/src/database/crud/suggestions.py
@@ -2,4 +2,5 @@ from sqlalchemy.orm import Session
 
 from src.database.models import Suggestion, Student, User
 
-def create_suggestion(database_session: Session, )
+def create_suggestion(database_session: Session):
+    pass
--- backend/tests/test_database/test_crud/test_suggestions.py
+++ backend/tests/test_database/test_crud/test_suggestions.py
@@ -3,3 +3,12 @@ from sqlalchemy.orm import Session
 from src.database.models import Suggestion, Student, User
 
 from src.database.crud.suggestions import create_suggestion
+from tests.fill_database import fill_database
+
+def test_create_suggestion(database_session: Session):
+    fill_database(database_session)
+
+    user: User = database_session.query(User).where(User.name == "coach1").first()
+    print(user)
+
+    assert False
