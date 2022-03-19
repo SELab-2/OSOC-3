aa28cf2fa5a0d4ab4a548a817d53517cdb8066da
Tiebe Vercoutter <tiebe.vercoutter@ugent.be>
2022-03-19 15:17:43 +0100

    started working on new log in page

--- frontend/src/views/LoginPage.tsx
+++ frontend/src/views/LoginPage.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import LogInButtons from "../components/LogInButtons";
 import logoO1 from "../images/letters/osoc_orange_o.svg"
 import logoS from "../images/letters/osoc_s.svg"
 import logoO2 from "../images/letters/osoc_red_o.svg"
 import logoC from "../images/letters/osoc_c.svg"
+import { Link } from "react-router-dom";
+
+import {
+    FacebookLoginButton,
+    GoogleLoginButton,
+    GithubLoginButton,
+  } from "react-social-login-buttons";
+
 function LoginPage() {
     return(
         <div>
@@ -16,7 +23,40 @@ function LoginPage() {
                     <h1 style={{marginBottom:'40px'}}>Hi!</h1>
                     <h3>Welcome to the open Summer of Code selections app. After you've logged in with your account, we'll enable your account so you can get started. An admin will verify you as quick as possible</h3>
                 </div>
-                <LogInButtons/>
+                <h2 style={{marginBottom:'40px', marginTop:'40px'}}>Log in</h2>
+                <div className="login">
+                    <div className="socials">
+                        <FacebookLoginButton/>
+                        <GoogleLoginButton/>
+                        <GithubLoginButton/>
+        
+                    </div>
+                    <div className="border-right"/>
+
+                    <div className="register-form-input-fields">
+                        <div>
+                            <input type="email" name="email" placeholder="Email"/>
+                        </div>
+                        <div>
+                            <input type="password" name="password" placeholder="Password"/>
+                        </div>
+                        <div className="no-account">
+                            <Link to="/signup">
+                                Don't have an account? Click here to sign up.
+                            </Link>
+                        </div>
+                        <div className="remember-and-reset">
+                            <input type="checkbox" id="checkbox"/>
+                            <label htmlFor="checkbox">Remember me</label>
+
+                            <Link to="/home">Reset Password</Link>
+                        </div>
+                        <div>
+                            <button className="register-button">Log In</button>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
