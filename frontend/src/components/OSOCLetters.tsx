56a1f9290277e302a06648e6e6022dc25aeb1d79
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-20 15:08:11 +0100

    made an osoc letters component for easy reuse

---
+++ frontend/src/components/OSOCLetters.tsx
@@ -0,0 +1,17 @@
+import logoO1 from "../images/letters/osoc_orange_o.svg"
+import logoS from "../images/letters/osoc_s.svg"
+import logoO2 from "../images/letters/osoc_red_o.svg"
+import logoC from "../images/letters/osoc_c.svg"
+
+function OSOCLetters() {
+    return(
+        <div className="osoc-letters">
+            <img src={logoO1} alt="logoO1" className="osoc-logo-O1"/>
+            <img src={logoS} alt="logoS" className="osoc-logo-S"/>
+            <img src={logoO2} alt="logoO2" className="osoc-logo-O2"/>
+            <img src={logoC} alt="logoC" className="osoc-logo-C"/>
+        </div>
+    )
+}
+
+export default OSOCLetters
--- frontend/src/views/LoginPage.tsx
+++ frontend/src/views/LoginPage.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import logoO1 from "../images/letters/osoc_orange_o.svg"
-import logoS from "../images/letters/osoc_s.svg"
-import logoO2 from "../images/letters/osoc_red_o.svg"
-import logoC from "../images/letters/osoc_c.svg"
+import OSOCLetters from "../components/OSOCLetters";
 
 import {
     GoogleLoginButton,
@@ -12,10 +9,7 @@ import {
 function LoginPage() {
     return(
         <div>
-            <img src={logoO1} alt="logoO1" className="osoc-logo-O1"/>
-            <img src={logoS} alt="logoS" className="osoc-logo-S"/>
-            <img src={logoO2} alt="logoO2" className="osoc-logo-O2"/>
-            <img src={logoC} alt="logoC" className="osoc-logo-C"/>
+            <OSOCLetters/>
             <div className="login-page-content-container">
                 <div className="welcome-text">
                     <h1 style={{marginBottom:'40px'}}>Hi!</h1>
