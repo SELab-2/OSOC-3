7b821be35c5f38d50e768bf6f7dd1101ec68c433
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-20 15:08:11 +0100

    made an osoc letters component for easy reuse

--- frontend/src/views/PendingPage.tsx
+++ frontend/src/views/PendingPage.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import logoO1 from "../images/letters/osoc_orange_o.svg";
-import logoS from "../images/letters/osoc_s.svg";
-import logoO2 from "../images/letters/osoc_red_o.svg";
-import logoC from "../images/letters/osoc_c.svg";
+import OSOCLetters from "../components/OSOCLetters";
+
 function PendingPage() {
     return(
         <div>
-            <img src={logoO1} alt="logoO1" className="osoc-logo-O1"/>
-            <img src={logoS} alt="logoS" className="osoc-logo-S"/>
-            <img src={logoO2} alt="logoO2" className="osoc-logo-O2"/>
-            <img src={logoC} alt="logoC" className="osoc-logo-C"/>
+            <OSOCLetters/>
             <div className="pending-page-content-container">
                 <div className="pending-page-content">
                     <span>Your request is pending...</span>
