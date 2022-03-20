c3a92d2128062a037cda491b93baff6db9134169
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-20 18:35:25 +0100

    added axios still need to wait for CROSS

--- frontend/src/views/LoginPage.tsx
+++ frontend/src/views/LoginPage.tsx
@@ -1,49 +1,58 @@
-import React from "react";
 import OSOCLetters from "../components/OSOCLetters";
 
-import {
-    GoogleLoginButton,
-    GithubLoginButton,
-  } from "react-social-login-buttons";
+import { GoogleLoginButton, GithubLoginButton } from "react-social-login-buttons";
+
+const axios = require("axios").default;
 
 function LoginPage() {
-    return(
+
+    function logIn(){
+        axios.get('http://127.0.0.1:8000/login/token').then((response: any) => console.log(response));
+    }
+
+    return (
         <div>
-            <OSOCLetters/>
+            <OSOCLetters />
             <div className="login-page-content-container">
                 <div className="welcome-text">
-                    <h1 style={{marginBottom:'40px'}}>Hi!</h1>
-                    <h3>Welcome to the open Summer of Code selections app. After you've logged in with your account, we'll enable your account so you can get started. An admin will verify you as quick as possible</h3>
+                    <h1 style={{ marginBottom: "40px" }}>Hi!</h1>
+                    <h3>
+                        Welcome to the open Summer of Code selections app. After you've logged in
+                        with your account, we'll enable your account so you can get started. An
+                        admin will verify you as quick as possible
+                    </h3>
                 </div>
                 <div className="login">
                     <div className="socials-container">
                         <div className="socials">
                             <div className="google-login-container">
-                                <GoogleLoginButton/>
+                                <GoogleLoginButton />
                             </div>
-                            <GithubLoginButton/>
+                            <GithubLoginButton />
                         </div>
                     </div>
-                    <div className="border-right"/>
+                    <div className="border-right" />
 
                     <div className="register-form-input-fields">
                         <div>
-                            <input type="email" name="email" placeholder="Email"/>
+                            <input type="email" name="email" placeholder="Email" />
                         </div>
                         <div>
-                            <input type="password" name="password" placeholder="Password"/>
+                            <input type="password" name="password" placeholder="Password" />
                         </div>
                         <div className="no-account">
                             Don't have an account? Ask an admin for an invite link
                         </div>
                         <div>
-                            <button className="login-button">Log In</button>
+                            <button className="login-button" onClick={logIn}>Log In</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default LoginPage
+export default LoginPage;
+
+
