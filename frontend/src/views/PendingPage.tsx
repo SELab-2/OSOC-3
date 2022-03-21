9b5b7bc0020edf9fe04b6b17cdeb9fe56a54ab8f
Clement Deloof <clement.deloof@Ugent.be>
2022-03-21 04:38:38 +0100

    added something fun

--- frontend/src/views/PendingPage.tsx
+++ frontend/src/views/PendingPage.tsx
@@ -7,8 +7,13 @@ function PendingPage() {
             <OSOCLetters/>
             <div className="pending-page-content-container">
                 <div className="pending-page-content">
-                    <span>Your request is pending...</span>
-                    <span>Please wait until an admin approves your request!</span>
+                    <div className="pending-text">
+                        <h1>Your request is pending</h1>
+                        <h1 className="pending-dot-1">.</h1>
+                        <h1 className="pending-dot-2">.</h1>
+                        <h1 className="pending-dot-3">.</h1>
+                    </div>
+                    <h1>Please wait until an admin approves your request!</h1>
                 </div>
             </div>
         </div>
