ffce2831f16e03ffb1dddf202ff366079329c405
Clement Deloof <clement.deloof@Ugent.be>
2022-03-21 04:49:36 +0100

    fix alignment

--- frontend/src/views/PendingPage.tsx
+++ frontend/src/views/PendingPage.tsx
@@ -7,13 +7,17 @@ function PendingPage() {
             <OSOCLetters/>
             <div className="pending-page-content-container">
                 <div className="pending-page-content">
-                    <div className="pending-text">
-                        <h1>Your request is pending</h1>
-                        <h1 className="pending-dot-1">.</h1>
-                        <h1 className="pending-dot-2">.</h1>
-                        <h1 className="pending-dot-3">.</h1>
+                    <div className="pending-text-container">
+                        <div className="pending-text-content">
+                            <h1>Your request is pending</h1>
+                            <h1 className="pending-dot-1">.</h1>
+                            <h1 className="pending-dot-2">.</h1>
+                            <h1 className="pending-dot-3">.</h1>
+                        </div>
                     </div>
-                    <h1>Please wait until an admin approves your request!</h1>
+                    <div>
+                        <h1>Please wait until an admin approves your request!</h1>
+                    </div>
                 </div>
             </div>
         </div>
