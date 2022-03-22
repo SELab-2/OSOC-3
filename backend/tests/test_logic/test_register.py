cdcad292a2eb8c7a124934d806f7b8d8a16eac62
Ward Meersman <wardmeersman@gmail.com>
2022-03-22 14:46:27 +0100

    fixed failed tests

--- backend/tests/test_logic/test_register.py
+++ backend/tests/test_logic/test_register.py
@@ -2,7 +2,7 @@ import pytest
 from sqlalchemy.orm import Session
 
 from src.app.schemas.register import NewUser
-from src.database.models import AuthEmail, CoachRequest, User, Edition
+from src.database.models import AuthEmail, CoachRequest, User, Edition, InviteLink
 
 from src.app.logic.register import create_request
 from src.app.exceptions.register import FailedToAddNewUserException
@@ -12,9 +12,11 @@ def test_create_request(database_session: Session):
     """Tests if a normal request can be created"""
     edition = Edition(year=2022)
     database_session.add(edition)
+    invite_link: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
     database_session.commit()
-    nu = NewUser(name="jos", email="email@email.com", pw="wachtwoord")
-    create_request(database_session, nu, edition)
+    new_user = NewUser(name="jos", email="email@email.com", pw="wachtwoord", uuid=invite_link.uuid)
+    create_request(database_session, new_user, edition)
 
     users = database_session.query(User).where(User.name == "jos").all()
     assert len(users) == 1
@@ -23,7 +25,7 @@ def test_create_request(database_session: Session):
     auth_email = database_session.query(AuthEmail).where(
         AuthEmail.user == users[0]).all()
     assert len(coach_requests) == 1
-    assert auth_email[0].pw_hash != nu.pw
+    assert auth_email[0].pw_hash != new_user.pw
     assert len(auth_email) == 1
 
 
@@ -31,12 +33,29 @@ def test_duplicate_user(database_session: Session):
     """Tests if there is a duplicate, it's not created in the database"""
     edition = Edition(year=2022)
     database_session.add(edition)
+    invite_link1: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
+    invite_link2: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
     database_session.commit()
-    nu1 = NewUser(name="user1", email="email@email.com", pw="wachtwoord1")
-    nu2 = NewUser(name="user2", email="email@email.com", pw="wachtwoord2")
+    nu1 = NewUser(name="user1", email="email@email.com", pw="wachtwoord1", uuid=invite_link1.uuid)
+    nu2 = NewUser(name="user2", email="email@email.com", pw="wachtwoord2", uuid=invite_link2.uuid)
     create_request(database_session, nu1, edition)
     with pytest.raises(FailedToAddNewUserException):
         create_request(database_session, nu2, edition)
+
+def test_use_same_uuid_multiple_times(database_session: Session):
+    """Tests that you can't use the same UUID multiple times"""
+    edition = Edition(year=2022)
+    database_session.add(edition)
+    invite_link: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
+    database_session.commit()
+    new_user1 = NewUser(name="jos", email="email@email.com", pw="wachtwoord", uuid=invite_link.uuid)
+    create_request(database_session, new_user1, edition)
+    with pytest.raises(FailedToAddNewUserException):
+        new_user2 = NewUser(name="jos", email="email2@email.com", pw="wachtwoord", uuid=invite_link.uuid)
+        create_request(database_session, new_user2, edition)
 
 
 def test_not_a_correct_email(database_session: Session):
@@ -45,5 +64,5 @@ def test_not_a_correct_email(database_session: Session):
     database_session.add(edition)
     database_session.commit()
     with pytest.raises(ValueError):
-        nu = NewUser(name="jos", email="email", pw="wachtwoord")
-        create_request(database_session, nu, edition)
+        new_user = NewUser(name="jos", email="email", pw="wachtwoord")
+        create_request(database_session, new_user, edition)
