9c3114904926be1811b6e8f951e718536b3f9340
Ward Meersman <wardmeersman@gmail.com>
2022-03-22 14:31:21 +0100

    register is based on a UUID

--- backend/src/app/schemas/register.py
+++ backend/src/app/schemas/register.py
@@ -1,5 +1,9 @@
+from uuid import UUID
 from src.app.schemas.invites import EmailAddress
 
+
 class NewUser(EmailAddress):
+    """The scheme of a new user"""
     name: str
     pw: str
+    uuid: UUID
--- backend/tests/test_routers/test_editions/test_register/test_register.py
+++ backend/tests/test_routers/test_editions/test_register/test_register.py
@@ -2,16 +2,55 @@ from sqlalchemy.orm import Session
 from starlette import status
 from starlette.testclient import TestClient
 
-from src.database.models import Edition
+from src.database.models import Edition, InviteLink, User
 
 
 def test_ok(database_session: Session, test_client: TestClient):
     """Tests a registeration is made"""
-    database_session.add(Edition(year=2022))
+    edition: Edition = Edition(year=2022)
+    invite_link: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
+    database_session.add(edition)
+    database_session.add(invite_link)
     database_session.commit()
     response = test_client.post("/editions/1/register/email", json={
-                                "name": "Joskes vermeulen", "email": "jw@gmail.com", "pw": "test"})
+                                "name": "Joskes vermeulen", "email": "jw@gmail.com", "pw": "test",
+                                "uuid": str(invite_link.uuid)})
     assert response.status_code == status.HTTP_201_CREATED
+    user: User = database_session.query(User).where(
+        User.email == "jw@gmail.com").one()
+    assert user.name == "Joskes vermeulen"
+
+
+def test_use_uuid_multipli_times(database_session: Session, test_client: TestClient):
+    """Tests that you can't use the same UUID multiple times"""
+    edition: Edition = Edition(year=2022)
+    invite_link: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
+    database_session.add(edition)
+    database_session.add(invite_link)
+    database_session.commit()
+    test_client.post("/editions/1/register/email", json={
+        "name": "Joskes vermeulen", "email": "jw@gmail.com", "pw": "test",
+        "uuid": str(invite_link.uuid)})
+    response = test_client.post("/editions/1/register/email", json={
+                                "name": "Joske Vermeulen", "email": "jw2@gmail.com", "pw": "test",
+                                "uuid": str(invite_link.uuid)})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_no_valid_uuid(database_session: Session, test_client: TestClient):
+    """Tests that no valid uuid, can't make a account"""
+    edition: Edition = Edition(year=2022)
+    database_session.add(edition)
+    database_session.commit()
+    response = test_client.post("/editions/1/register/email", json={
+                                "name": "Joskes vermeulen", "email": "jw@gmail.com", "pw": "test",
+                                "uuid": "550e8400-e29b-41d4-a716-446655440000"})
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    users: list[User] = database_session.query(
+        User).where(User.email == "jw@gmail.com").all()
+    assert len(users) == 0
 
 
 def test_no_edition(database_session: Session, test_client: TestClient):
@@ -30,12 +69,21 @@ def test_not_a_correct_email(database_session: Session, test_client: TestClient)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_duplicate(database_session: Session, test_client: TestClient):
+def test_duplicate_user(database_session: Session, test_client: TestClient):
     """Tests when there is a duplicate, it gets the right error code"""
-    database_session.add(Edition(year=2022))
+    edition: Edition = Edition(year=2022)
+    invite_link1: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
+    invite_link2: InviteLink = InviteLink(
+        edition=edition, target_email="jw@gmail.com")
+    database_session.add(edition)
+    database_session.add(invite_link1)
+    database_session.add(invite_link2)
     database_session.commit()
     test_client.post("/editions/1/register/email",
-                     json={"name": "Joskes vermeulen", "email": "jw@gmail.com", "pw": "test"})
+                     json={"name": "Joskes vermeulen", "email": "jw@gmail.com", "pw": "test",
+                           "uuid": str(invite_link1.uuid)})
     response = test_client.post("/editions/1/register/email", json={
-                                "name": "Joske vermeulen", "email": "jw@gmail.com", "pw": "test1"})
+                                "name": "Joske vermeulen", "email": "jw@gmail.com", "pw": "test1",
+                                "uuid": str(invite_link2.uuid)})
     assert response.status_code == status.HTTP_400_BAD_REQUEST
