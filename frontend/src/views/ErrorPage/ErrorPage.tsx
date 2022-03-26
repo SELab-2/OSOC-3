7d87b316df5744bd5ff4f7656d3f675667c07bf0
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-26 12:35:07 +0100

    refactor error page

---
+++ frontend/src/views/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ErrorPage.css";
+
+function ErrorPage() {
+    return (
+        <h1 className="error">
+            Oops! This is awkward... You are looking for something that doesn't actually exists.
+        </h1>
+    );
+}
+
+export default ErrorPage;
