67b0ad60f2451a3dda83764d00d7c15f71d4f8c5
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-26 14:03:46 +0100

    every page has its directory and index file

--- frontend/src/views/RegisterPage/RegisterPage.tsx
+++ frontend/src/views/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { axiosInstance } from "../utils/api/api";
-import OSOCLetters from "../components/OSOCLetters/OSOCLetters";
+import { axiosInstance } from "../../utils/api/api";
+import OSOCLetters from "../../components/OSOCLetters/OSOCLetters";
+import "./RegisterPage.css";
 
 import { GoogleLoginButton, GithubLoginButton } from "react-social-login-buttons";
 
