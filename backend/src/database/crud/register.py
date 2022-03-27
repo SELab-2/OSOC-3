733306f1126d630655979c9c5cf18f7e36dd61f8
stijndcl <declercq.stijn@outlook.com>
2022-03-27 19:15:47 +0200

    Rewrite usage of transactions, update test

--- backend/src/database/crud/register.py
+++ backend/src/database/crud/register.py
@@ -1,42 +1,36 @@
-
 from sqlalchemy.orm import Session
 
-from src.app.exceptions.register import FailedToAddNewUserException
 from src.database.models import AuthEmail, CoachRequest, User, Edition
 
 
-def create_user(db: Session, name: str) -> User:
+def create_user(db: Session, name: str, commit: bool = True) -> User:
     """Create a user"""
     new_user: User = User(name=name)
     db.add(new_user)
-    db.commit()
+
+    if commit:
+        db.commit()
+
     return new_user
 
 
-def create_coach_request(db: Session, user: User, edition: Edition) -> CoachRequest:
+def create_coach_request(db: Session, user: User, edition: Edition, commit: bool = True) -> CoachRequest:
     """Create a coach request"""
     coach_request: CoachRequest = CoachRequest(user=user, edition=edition)
     db.add(coach_request)
-    db.commit()
+
+    if commit:
+        db.commit()
+
     return coach_request
 
 
-def create_auth_email(db: Session, user: User, pw_hash: str, email: str) -> AuthEmail:
+def create_auth_email(db: Session, user: User, pw_hash: str, email: str, commit: bool = True) -> AuthEmail:
     """Create a authentication for email"""
     auth_email: AuthEmail = AuthEmail(user=user, pw_hash=pw_hash, email=email)
     db.add(auth_email)
-    db.commit()
-    return auth_email
 
-
-def create_user_with_auth(db: Session, name: str, pw_hash: str, email: str) -> User:
-    new_user: User = User(name=name)
-    db.add(new_user)
-    auth_email: AuthEmail = AuthEmail(user=new_user, pw_hash=pw_hash, email=email)
-    db.add(auth_email)
-    try:
+    if commit:
         db.commit()
-    except Exception as exception:
-        raise FailedToAddNewUserException from exception
 
-    return new_user
+    return auth_email
