88295b12b3229b84fce15db4f21fa1bd1db3a686
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-27 15:46:37 +0200

    Making seperate components for login page

---
+++ frontend/src/views/LoginPage/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const VerticalDivider = styled.div`
+    margin-right: 20px;
+    padding-right: 20px;
+    border-right: 2px solid rgba(182, 182, 182, 0.603);
+`;
