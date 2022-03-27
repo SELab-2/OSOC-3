967a280fe51227b7ea5708a30e0246ee70af8967
SeppeM8 <seppe.maes8@gmail.com>
2022-03-27 15:16:14 +0200

    Refactor InviteUser.tsx

--- frontend/src/views/UsersPage/InviteUser/InviteUser.tsx
+++ frontend/src/views/UsersPage/InviteUser/InviteUser.tsx
@@ -3,6 +3,36 @@ import { getInviteLink } from "../../../utils/api/users";
 import "./InviteUsers.css";
 import { InviteInput, InviteButton, Loader, InviteContainer, Link, Error } from "./styles";
 
+function ButtonDiv(props: { loading: boolean; onClick: () => void }) {
+    let buttonDiv;
+    if (props.loading) {
+        buttonDiv = <Loader />;
+    } else {
+        buttonDiv = (
+            <div>
+                <InviteButton onClick={props.onClick}>Send invite</InviteButton>
+            </div>
+        );
+    }
+    return buttonDiv;
+}
+
+function ErrorDiv(props: { errorMessage: string }) {
+    let errorDiv = null;
+    if (props.errorMessage) {
+        errorDiv = <Error>{props.errorMessage}</Error>;
+    }
+    return errorDiv;
+}
+
+function LinkDiv(props: { link: string }) {
+    let linkDiv = null;
+    if (props.link) {
+        linkDiv = <Link>{props.link}</Link>;
+    }
+    return linkDiv;
+}
+
 export default function InviteUser() {
     const [email, setEmail] = useState("");
     const [valid, setValid] = useState(true);
@@ -20,45 +50,14 @@ export default function InviteUser() {
     const sendInvite = async () => {
         if (/[^@\s]+@[^@\s]+\.[^@\s]+/.test(email)) {
             setLoading(true);
-            getInviteLink("edition", email).then(ding => {
-                setLink(ding);
-                setLoading(false);
-                // TODO: fix email stuff
-            });
+            const ding = await getInviteLink("edition", email);
+            setLink(ding);
+            setLoading(false);
+            // TODO: fix email stuff
         } else {
             setValid(false);
             setErrorMessage("Invalid email");
         }
-    };
-
-    const buttonDiv = () => {
-        let button;
-        if (loading) {
-            button = <Loader />;
-        } else {
-            button = (
-                <div>
-                    <InviteButton onClick={() => sendInvite()}>Send invite</InviteButton>
-                </div>
-            );
-        }
-        return button;
-    };
-
-    const errorDiv = () => {
-        let errorDiv = null;
-        if (errorMessage) {
-            errorDiv = <Error>{errorMessage}</Error>;
-        }
-        return errorDiv;
-    };
-
-    const linkDiv = () => {
-        let linkDiv = null;
-        if (link) {
-            linkDiv = <Link>{link}</Link>;
-        }
-        return linkDiv;
     };
 
     return (
@@ -66,14 +65,13 @@ export default function InviteUser() {
             <InviteContainer>
                 <InviteInput
                     className={valid ? "" : "email-field-error"}
-                    placeholder="Invite user by email"
                     value={email}
                     onChange={e => changeEmail(e.target.value)}
                 />
-                {buttonDiv()}
+                <ButtonDiv loading={loading} onClick={sendInvite} />
             </InviteContainer>
-            {errorDiv()}
-            {linkDiv()}
+            <ErrorDiv errorMessage={errorMessage} />
+            <LinkDiv link={link} />
         </div>
     );
 }
