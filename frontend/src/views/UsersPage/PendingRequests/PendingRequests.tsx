7c0ee3af75ba52e4f9bc017a41af4ffe612416e7
SeppeM8 <seppe.maes8@gmail.com>
2022-03-27 21:17:44 +0200

    Filter Pending Requests

--- frontend/src/views/UsersPage/PendingRequests/PendingRequests.tsx
+++ frontend/src/views/UsersPage/PendingRequests/PendingRequests.tsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Collapsible from "react-collapsible";
 import {
-    RequestHeader,
+    RequestHeaderTitle,
     RequestsTable,
     PendingRequestsContainer,
     AcceptButton,
     RejectButton,
+    SpinnerContainer,
+    SearchInput,
 } from "./styles";
 import { acceptRequest, getRequests, rejectRequest, Request } from "../../../utils/api/users";
 import { Spinner } from "react-bootstrap";
 
-function RequestsHeader() {
-    // TODO: Search field when out-folded
-    return <RequestHeader>Requests</RequestHeader>;
+function RequestHeader(props: { open: boolean }) {
+    return <RequestHeaderTitle>Requests {props.open ? "opened" : "closed"}</RequestHeaderTitle>;
+}
+
+function RequestFilter(props: {
+    search: boolean;
+    searchTerm: string;
+    filter: (key: string) => void;
+}) {
+    return <SearchInput value={props.searchTerm} onChange={e => props.filter(e.target.value)} />;
 }
 
 function AcceptReject(props: { request_id: number }) {
@@ -38,7 +47,11 @@ function RequestItem(props: { request: Request }) {
 
 function RequestsList(props: { requests: Request[]; loading: boolean }) {
     if (props.loading) {
-        return <Spinner animation="border" />;
+        return (
+            <SpinnerContainer>
+                <Spinner animation="border" />
+            </SpinnerContainer>
+        );
     } else if (props.requests.length === 0) {
         return <div>No requests</div>;
     }
@@ -50,7 +63,6 @@ function RequestsList(props: { requests: Request[]; loading: boolean }) {
             ))}
         </tbody>
     );
-    props.requests.map(request => <RequestItem request={request} />);
 
     return (
         <RequestsTable variant="dark">
@@ -67,24 +79,56 @@ function RequestsList(props: { requests: Request[]; loading: boolean }) {
 }
 
 export default function PendingRequests(props: { edition: string | undefined }) {
+    const [allRequests, setAllRequests] = useState<Request[]>([]);
     const [requests, setRequests] = useState<Request[]>([]);
     const [gettingRequests, setGettingRequests] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [gotData, setGotData] = useState(false);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
-        getRequests(props.edition)
-            .then(response => {
-                setRequests(response.requests);
-                setGettingRequests(false);
-            })
-            .catch(function (error: any) {
-                console.log(error);
-                setGettingRequests(false);
-            });
+        if (!gotData) {
+            getRequests(props.edition)
+                .then(response => {
+                    setRequests(response.requests);
+                    setAllRequests(response.requests);
+                    setGettingRequests(false);
+                    setGotData(true);
+                })
+                .catch(function (error: any) {
+                    console.log(error);
+                    setGettingRequests(false);
+                });
+        }
     });
 
+    const filter = (word: string) => {
+        setSearchTerm(word);
+        const newRequests: Request[] = [];
+        for (const request of allRequests) {
+            if (
+                request.user.name.toUpperCase().includes(word.toUpperCase()) ||
+                request.user.email.toUpperCase().includes(word.toUpperCase())
+            ) {
+                newRequests.push(request);
+            }
+        }
+        setRequests(newRequests);
+    };
+
+    // @ts-ignore
     return (
         <PendingRequestsContainer>
-            <Collapsible trigger={<RequestsHeader />}>
+            <Collapsible
+                trigger={<RequestHeader open={open} />}
+                onOpen={() => setOpen(true)}
+                onClose={() => setOpen(false)}
+            >
+                <RequestFilter
+                    search={requests.length > 0}
+                    searchTerm={searchTerm}
+                    filter={word => filter(word)}
+                />
                 <RequestsList requests={requests} loading={gettingRequests} />
             </Collapsible>
         </PendingRequestsContainer>
