a7ba10520bf4925d5adffc2ddb14ae13aa258605
SeppeM8 <seppe.maes8@gmail.com>
2022-03-27 21:17:44 +0200

    Filter Pending Requests

--- frontend/src/views/UsersPage/PendingRequests/styles.ts
+++ frontend/src/views/UsersPage/PendingRequests/styles.ts
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 import { Table } from "react-bootstrap";
 
-export const RequestHeader = styled.div`
-    background-color: var(--osoc_red);
+export const RequestHeaderTitle = styled.div`
     padding-bottom: 3px;
     padding-left: 3px;
+    width: 100px;
+    font-size: 25px;
+`;
+
+export const SearchInput = styled.input.attrs({
+    placeholder: "Search",
+})`
+    margin: 3px;
+    height: 20px;
+    width: 150px;
+    font-size: 15px;
+    border-radius: 5px;
+    border-width: 0;
 `;
 
 export const RequestsTable = styled(Table)``;
@@ -28,4 +40,11 @@ export const RejectButton = styled.button`
     padding-bottom: 3px;
     padding-left: 3px;
     padding-right: 3px;
+`;
+
+export const SpinnerContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px;
 `;
