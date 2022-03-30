557169fbe52ece95ec7793eed67e1b08898f4afa
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-30 14:58:40 +0200

    More styled components for pending page

---
+++ frontend/src/views/PendingPage/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const PendingPageContainer = styled.div`
+    height: fit-content;
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+export const PendingContainer = styled.div`
+    max-width: 1200px;
+    text-align: center;
+    height: fit-content;
+`;
+
+export const PendingTextContainer = styled.div`
+    width: fit-content;
+    display: inline-block;
+`;
+
+export const PendingText = styled.div`
+    display: flex;
+`;
