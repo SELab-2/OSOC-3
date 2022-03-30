a9887fe5e99e90894c1ddb6e9812348bffbf0307
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-30 14:59:53 +0200

    Removed inline style for project page

--- frontend/src/views/ProjectsPage/ProjectsPage.tsx
+++ frontend/src/views/ProjectsPage/ProjectsPage.tsx
@@ -2,18 +2,7 @@ import React from "react";
 import "./ProjectsPage.css";
 
 function ProjectPage() {
-    return (
-        <div
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "90vh",
-            }}
-        >
-            This is the projects page
-        </div>
-    );
+    return <div>This is the projects page</div>;
 }
 
 export default ProjectPage;
