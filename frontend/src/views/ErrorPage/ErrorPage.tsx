a7415e93db299472ff04f8d6574d0e0c97d66c51
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-03-30 15:01:36 +0200

    typo in errorpage

--- frontend/src/views/ErrorPage/ErrorPage.tsx
+++ frontend/src/views/ErrorPage/ErrorPage.tsx
@@ -4,7 +4,7 @@ import { ErrorMessage } from "./styles";
 function ErrorPage() {
     return (
         <ErrorMessage>
-            Oops! This is awkward... You are looking for something that doesn't actually exists.
+            Oops! This is awkward... You are looking for something that doesn't actually exist.
         </ErrorMessage>
     );
 }
