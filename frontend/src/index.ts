36812c0d88c04b87dcc6690196f75cbf88b28b8b
stijndcl <declercq.stijn@outlook.com>
2022-04-03 00:34:54 +0200

    Add all index files & exports

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,3 +1,5 @@
 export * as Components from "./components";
 export * as Contexts from "./contexts";
 export * as Data from "./data";
+export * as Utils from "./utils";
+export * as Views from "./views";
---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,2 @@
+export * as Api from "./api";
+export * as LocalStorage from "./local-storage";
