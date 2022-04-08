78f5823e09efca7854c2bfaf4a959c7eba52af65
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-04-08 20:31:34 +0200

    alembic upgrade

---
+++ backend/migrations/versions/964637070800_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 964637070800
+Revises: a4a047b881db, c5bdaa5815ca
+Create Date: 2022-04-08 20:25:41.099295
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '964637070800'
+down_revision = ('a4a047b881db', 'c5bdaa5815ca')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
