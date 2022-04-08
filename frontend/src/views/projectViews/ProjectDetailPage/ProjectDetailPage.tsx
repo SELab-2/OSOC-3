940d520da259216abdecad669e283a9af3e08cc4
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-04-08 21:46:26 +0200

    get project data

--- frontend/src/views/projectViews/ProjectDetailPage/ProjectDetailPage.tsx
+++ frontend/src/views/projectViews/ProjectDetailPage/ProjectDetailPage.tsx
@@ -1,8 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Project } from "../../../data/interfaces";
+
+import { getProject } from "../../../utils/api/projects";
 
 export default function ProjectDetailPage() {
     const params = useParams();
-    const projectId = params.projectId;
+    const projectId = parseInt(params.projectId!);
 
-    return <div>{projectId}</div>;
+    const [project, setProject] = useState<Project>();
+    const [gotProject, setGotProject] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function callProjects() {
+            if (projectId) {
+                setGotProject(true);
+                const response = await getProject("summerof2022", projectId);
+                if (response) {
+                    setProject(response);
+                } else navigate("/404-not-found");
+            }
+        }
+        if (!gotProject) {
+            callProjects();
+        }
+    });
+    if (project) {
+        return <div>{project.name}</div>;
+    } else return <div></div>;
 }
