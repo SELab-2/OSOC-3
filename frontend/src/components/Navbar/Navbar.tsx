4ae97a64ebddb1ad2d3ea07d29872940a8001724
stijndcl <declercq.stijn@outlook.com>
2022-04-11 15:07:00 +0200

    Parse edition out of URL & use sessionstorage otherwise

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -8,25 +8,44 @@ import EditionDropdown from "./EditionDropdown";
 import "./Navbar.css";
 import LogoutButton from "./LogoutButton";
 import { getCurrentEdition, setCurrentEdition } from "../../utils/session-storage/current-edition";
-import { useParams } from "react-router-dom";
+import { matchPath, useLocation } from "react-router-dom";
 
+/**
+ * Navbar component displayed at the top of the screen.
+ * If the user is not signed in, this is hidden automatically.
+ */
 export default function Navbar() {
     const { isLoggedIn, editions } = useAuth();
-    const params = useParams();
-
-    // If the current URL contains an edition, use that
-    // if not (eg. /editions), check SessionStorage
-    // otherwise, use the most-recent edition from the auth response
-    const currentEdition = params.editionId ? params.editionId : getCurrentEdition() || editions[0];
-
-    // Set the value of the new edition in SessionStorage if useful
-    if (currentEdition) {
-        setCurrentEdition(currentEdition);
-    }
+    /**
+     * Important: DO NOT MOVE THIS LINE UNDERNEATH THE RETURN!
+     * Placing an early return above a React hook (in this case, useLocation) causes
+     * memory leaks & other wonky issues that we'd rather avoid.
+     * The hook is only used below the return, but it HAS to be called here.
+     */
+    const location = useLocation();
 
     // Don't render Navbar if not logged in
     if (!isLoggedIn) {
         return null;
+    }
+
+    // User is logged in: safe to try and parse the location now
+
+    // Try to get the editionId out of the URL if it exists
+    // this can not be done using useParams() because the Navbar is not inside
+    // a <Route/>
+    const match = matchPath({ path: "/editions/:editionId/*" }, location.pathname);
+    // This is a TypeScript shortcut for 3 if-statements
+    const editionId = match && match.params && match.params.editionId;
+
+    // If the current URL contains an edition, use that
+    // if not (eg. /editions), check SessionStorage
+    // otherwise, use the most-recent edition from the auth response
+    const currentEdition = editionId || getCurrentEdition() || editions[0];
+
+    // Set the value of the new edition in SessionStorage if useful
+    if (currentEdition) {
+        setCurrentEdition(currentEdition);
     }
 
     return (
