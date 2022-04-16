93b6c8d3f7f899d2ac9398c17e0e3249fac4af67
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-04-16 19:07:19 +0200

    Created the route for a new project

--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -1,8 +1,7 @@
 export * as Errors from "./errors";
 export { default as LoginPage } from "./LoginPage";
 export { default as PendingPage } from "./PendingPage";
-export { ProjectsPage } from "./projectViews";
-export { ProjectDetailPage } from "./projectViews"
+export { ProjectsPage, ProjectDetailPage, CreateProjectPage } from "./projectViews";
 export { default as RegisterPage } from "./RegisterPage";
 export { default as StudentsPage } from "./StudentsPage";
 export { default as UsersPage } from "./UsersPage";
---
+++ frontend/src/views/projectViews/CreateProjectPage/CreateProjectPage.tsx
@@ -0,0 +1,8 @@
+export default function CreateProjectPage() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
