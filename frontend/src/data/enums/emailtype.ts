16963f74d65927c3309498f2f5b873c2afd60ef1
beguille <bert.guillemyn@ugent.be>
2022-04-16 17:02:53 +0200

    added extra email types

--- frontend/src/data/enums/emailtype.ts
+++ frontend/src/data/enums/emailtype.ts
@@ -2,8 +2,16 @@
  * Enum for the different types of emails that can be sent
  */
 export enum EmailType {
-    UNDECIDED = "Undecided",
-    YES = "Accepted",
-    MAYBE = "Maybe",
-    NO = "Denied",
+    // Nothing happened (undecided/screening)
+    APPLIED = "Applied",
+    // We're looking for a project (maybe)
+    AWAITING_PROJECT = "Awaiting Project",
+    // Can participate (yes)
+    APPROVED = "Approved",
+    // Student signed the contract
+    CONTRACT_CONFIRMED = "Contract Confirmed",
+    // Student indicated they don't want to participate anymore
+    CONTRACT_DECLINED = "Contract Declined",
+    // We've rejected the student ourselves (no)
+    REJECTED = "Rejected",
 }
--- frontend/src/utils/api/student_email_history.ts
+++ frontend/src/utils/api/student_email_history.ts
@@ -15,9 +15,9 @@ export async function getEmails(): Promise<EmailHistoryList> {
     // placeholder while the real API call is not available
     const data = {
         emails: [
-            { emailId: 1, date: "2022-04-14T12:36:28.641337", type: 0 },
+            { emailId: 1, date: "2022-04-13T11:46:28.641337", type: 0 },
             { emailId: 2, date: "2022-04-14T12:36:28.641337", type: 1 },
-            { emailId: 3, date: "2022-04-14T12:36:28.641337", type: 2 },
+            { emailId: 3, date: "2022-04-15T13:38:38.641337", type: 2 },
         ],
     };
     return data as EmailHistoryList;
