7ea80a532c90334f6f37a1ca5300c3790c88dbc0
FKD13 <francisklinck@gmail.com>
2022-04-18 12:39:17 +0200

    fix typing and linting

--- backend/src/database/crud/invites.py
+++ backend/src/database/crud/invites.py
@@ -39,6 +39,7 @@ def get_pending_invites_for_edition_page(db: Session, edition: Edition, page: in
 
 
 def get_optional_invite_link_by_edition_and_email(db: Session, edition: Edition, email: str) -> InviteLink | None:
+    """Return an optional invite link by edition and target_email"""
     return db.query(InviteLink).where(InviteLink.edition == edition and InviteLink.target_email == email).one_or_none()
 
 
