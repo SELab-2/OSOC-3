072cafa36386539136da6b5c9c59c9e3fe52cc81
beguille <bert.guillemyn@ugent.be>
2022-04-21 15:58:03 +0200

    fixed stupid bug

--- frontend/src/utils/api/student_email_history.ts
+++ frontend/src/utils/api/student_email_history.ts
@@ -7,6 +7,6 @@ export async function getEmails(
     editionId: string | undefined,
     studentId: string | undefined
 ): Promise<EmailHistoryList> {
-    const response = await axiosInstance.get(`/edition/${editionId}/student/${studentId}/emails`);
+    const response = await axiosInstance.get(`/editions/${editionId}/students/${studentId}/emails`);
     return response.data as EmailHistoryList;
 }
