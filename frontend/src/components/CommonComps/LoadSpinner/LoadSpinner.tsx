8331e21513b69756b6028f6a102257e8129d043b
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-04-24 16:15:48 +0200

    Common load spinner everybody can use

---
+++ frontend/src/components/CommonComps/LoadSpinner/LoadSpinner.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "react-bootstrap";
+import { SpinnerContainer } from "./styles";
+export default function LoadSpinner({ show }: { show: boolean }) {
+    if (!show) return null;
+    return (
+        <SpinnerContainer>
+            <Spinner animation="border" />
+        </SpinnerContainer>
+    );
+}
