96f42880f14d2d631d3c40371482c11e1387cd6a
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-04-24 16:22:30 +0200

    add documentation

--- frontend/src/components/CommonComps/LoadSpinner/LoadSpinner.tsx
+++ frontend/src/components/CommonComps/LoadSpinner/LoadSpinner.tsx
@@ -1,5 +1,10 @@
 import { Spinner } from "react-bootstrap";
 import { SpinnerContainer } from "./styles";
+/**
+ *
+ * @param show: whether to show the spinner or not
+ * @returns a spinner to display when data is being fetched
+ */
 export default function LoadSpinner({ show }: { show: boolean }) {
     if (!show) return null;
     return (
