b5cc2996ae5c2f41012a50b0c0eba56ead07e7b2
Tiebe Vercoutter <tiebe.vercoutter@protonmail.com>
2022-04-28 18:13:26 +0200

    clear coach field when add button is clicked QOL

--- frontend/src/components/ProjectsComponents/CreateProjectComponents/InputFields/Coach/Coach.tsx
+++ frontend/src/components/ProjectsComponents/CreateProjectComponents/InputFields/Coach/Coach.tsx
@@ -60,6 +60,7 @@ export default function Coach({
                             setShowAlert(false);
                         }
                     } else setShowAlert(true);
+                    setCoach("");
                 }}
             >
                 Add coach
