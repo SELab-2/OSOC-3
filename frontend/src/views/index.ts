cdba47adddd9e89972e6e349dbc807f64751445a
stijndcl <declercq.stijn@outlook.com>
2022-04-29 09:12:33 +0200

    Remove duplicate import

--- frontend/src/views/index.ts
+++ frontend/src/views/index.ts
@@ -5,7 +5,6 @@ export { default as EditionsPage } from "./EditionsPage";
 export { default as CreateEditionPage } from "./CreateEditionPage";
 export { default as PendingPage } from "./PendingPage";
 export { ProjectsPage, ProjectDetailPage, CreateProjectPage } from "./projectViews";
-export { default as RegisterPage } from "./RegisterPage";
 export { default as StudentsPage } from "./StudentsPage";
 export { default as UsersPage } from "./UsersPage";
 export { default as AdminsPage } from "./AdminsPage";
