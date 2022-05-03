16ce8d5b917b55e1e838e786d4bd0b714b78d93b
Ward Meersman <wardmeersman@gmail.com>
2022-05-03 21:37:47 +0200

    render tests LoginPage

--- frontend/src/views/LoginPage/LoginPage.test.js
+++ frontend/src/views/LoginPage/LoginPage.test.js
@@ -8,25 +8,28 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => (jest.fn())
 }));
 
-describe("renders all components", () => {
 
-    beforeEach(() => {
-        render(<LoginPage />);
-    })
+beforeEach(() => {
+    render(<LoginPage />);
+})
 
-    it('has an email field and it changes', () => {
-        const emailField = screen.getByPlaceholderText("Email");
-        expect(emailField).toHaveValue("")
-        userEvent.type(emailField, "email@email.com");
-        expect(emailField).toHaveValue("email@email.com")
-    })
+test('has an email field and it changes', () => {
+    const emailField = screen.getByPlaceholderText("Email");
+    expect(emailField).toHaveValue("")
+    userEvent.type(emailField, "email@email.com");
+    expect(emailField).toHaveValue("email@email.com")
+})
 
-    it('has an password field and it changes', () => {
-        const passwordField = screen.getByPlaceholderText("Password");
-        expect(passwordField).toHaveValue("")
-        userEvent.type(passwordField, "wachtwoord");
-        expect(passwordField).toHaveValue("wachtwoord")
-        fireEvent.keyPress(passwordField, { key: "Enter", code: 13, charCode: 13 })
-    })
+test('has an password field and it changes', () => {
+    const passwordField = screen.getByPlaceholderText("Password");
+    expect(passwordField).toHaveValue("")
+    userEvent.type(passwordField, "wachtwoord");
+    expect(passwordField).toHaveValue("wachtwoord")
+})
+
+test('has headings', () => {
+    const headings = screen.getAllByRole('heading')
+    expect(headings[0]).toHaveTextContent("Hi!")
+    expect(headings[1]).toHaveTextContent("Welcome to the Open Summer of Code selections app. After you've logged in with your account, we'll enable your account so you can get started. An admin will verify you as soon as possible.")
 })
 
