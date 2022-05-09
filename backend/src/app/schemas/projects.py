7272b5411830c838b4b57e08a739cf07e32ce24f
Francis <francisklinck@gmail.com>
2022-05-09 15:39:40 +0200

    remove comented property

--- backend/src/app/schemas/projects.py
+++ backend/src/app/schemas/projects.py
@@ -67,7 +67,6 @@ class Project(CamelCaseModel):
     """Represents a Project from the database to return when a GET request happens"""
     project_id: int
     name: str
-    # number_of_students: int
 
     coaches: list[User]
     partners: list[Partner]
