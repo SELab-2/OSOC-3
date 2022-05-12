91bdcfd52c8897dfd4b7439cf1d403c3e3bc530e
Ward Meersman <wardmeersman@gmail.com>
2022-05-12 11:36:51 +0200

    added answers

--- backend/src/app/routers/editions/students/answers/answers.py
+++ backend/src/app/routers/editions/students/answers/answers.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from starlette import status
+from src.app.logic.answers import gives_question_and_answers
 from src.app.routers.tags import Tags
-from src.app.utils.dependencies import get_student
+from src.app.utils.dependencies import get_student, require_auth
+from src.app.schemas.answers import Questions
 from src.database.models import Student
-from src.database.database import get_session
 
 students_answers_router = APIRouter(
     prefix="/answers", tags=[Tags.STUDENTS])
 
 
-@students_answers_router.get("/", status_code=status.HTTP_200_OK)
-async def get_answers(student: Student = Depends(get_student),
-                      db: AsyncSession = Depends(get_session)):
+@students_answers_router.get("/", status_code=status.HTTP_200_OK, response_model=Questions,
+                             dependencies=[Depends(require_auth)])
+async def get_answers(student: Student = Depends(get_student)):
     """give answers of a student"""
-    return student.alumni
+    return await gives_question_and_answers(student=student)
