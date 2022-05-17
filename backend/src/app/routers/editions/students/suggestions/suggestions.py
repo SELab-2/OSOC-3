dbaa41fe0125c690f9216fac1c83dcb440fad685
Ward Meersman <wardmeersman@gmail.com>
2022-05-17 09:17:18 +0200

    close #431

--- backend/src/app/routers/editions/students/suggestions/suggestions.py
+++ backend/src/app/routers/editions/students/suggestions/suggestions.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from starlette import status
 from src.app.routers.tags import Tags
-from src.app.utils.dependencies import require_auth, get_student, get_suggestion
+from src.app.utils.dependencies import get_latest_edition, require_auth, get_student, get_suggestion
 from src.database.database import get_session
 from src.database.models import Student, User, Suggestion
 from src.app.logic.suggestions import (make_new_suggestion, all_suggestions_of_student,
@@ -15,7 +15,8 @@ students_suggestions_router = APIRouter(
     prefix="/suggestions", tags=[Tags.STUDENTS])
 
 
-@students_suggestions_router.post("", status_code=status.HTTP_201_CREATED, response_model=SuggestionResponse)
+@students_suggestions_router.post("", status_code=status.HTTP_201_CREATED, response_model=SuggestionResponse,
+                                  dependencies=[Depends(get_latest_edition)])
 async def create_suggestion(new_suggestion: NewSuggestion, student: Student = Depends(get_student),
                             db: AsyncSession = Depends(get_session), user: User = Depends(require_auth)):
     """
@@ -37,7 +38,7 @@ async def delete_suggestion(db: AsyncSession = Depends(get_session), user: User 
 
 
 @students_suggestions_router.put("/{suggestion_id}", status_code=status.HTTP_204_NO_CONTENT,
-                                 dependencies=[Depends(get_student)])
+                                 dependencies=[Depends(get_student), Depends(get_latest_edition)])
 async def edit_suggestion(new_suggestion: NewSuggestion, db: AsyncSession = Depends(get_session),
                           user: User = Depends(require_auth), suggestion: Suggestion = Depends(get_suggestion)):
     """
