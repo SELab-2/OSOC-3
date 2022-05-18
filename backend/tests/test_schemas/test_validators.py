f907634487e751f5ac0a97e41c26c7c399680454
beguille <bert.guillemyn@ugent.be>
2022-05-18 20:20:36 +0200

    wrote tests to validate edition

--- backend/tests/test_schemas/test_validators.py
+++ backend/tests/test_schemas/test_validators.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.app.exceptions.validation_exception import ValidationException
-from src.app.schemas.validators import validate_email_format
+from src.app.schemas.validators import validate_email_format, validate_edition
 
 
 def test_email_address():
@@ -22,3 +22,23 @@ def test_email_address():
         validate_email_format("A@Couple@Of@@s")
 
     validate_email_format("test.some@thi.ng")
+
+
+def test_edition_name():
+    """Test the validation of edition names"""
+    with pytest.raises(ValidationException):
+        validate_edition("New Edition")
+
+    with pytest.raises(ValidationException):
+        validate_edition("NewEdition?!")
+
+    with pytest.raises(ValidationException):
+        validate_edition("(NewEdition)")
+
+    validate_edition("Edition2022")
+
+    validate_edition("Edition_2022")
+
+    validate_edition("edition2022")
+
+    validate_edition("Edition-2022")
