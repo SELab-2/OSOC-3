b72d6373ab4d62ed75141f189f3a2a6f9573ed31
Ward Meersman <wardmeersman@gmail.com>
2022-05-18 23:07:19 +0200

    use require_coach

--- backend/src/app/routers/editions/students/answers/answers.py
+++ backend/src/app/routers/editions/students/answers/answers.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from starlette import status
 from src.app.logic.answers import gives_question_and_answers
 from src.app.routers.tags import Tags
-from src.app.utils.dependencies import get_student, require_auth
+from src.app.utils.dependencies import get_student, require_coach
 from src.app.schemas.answers import Questions
 from src.database.models import Student
 
@@ -12,7 +12,7 @@ students_answers_router = APIRouter(
 
 
 @students_answers_router.get("/", status_code=status.HTTP_200_OK, response_model=Questions,
-                             dependencies=[Depends(require_auth)])
+                             dependencies=[Depends(require_coach)])
 async def get_answers(student: Student = Depends(get_student)):
     """give answers of a student"""
     return await gives_question_and_answers(student=student)
