092e62a17e7f695adb833a6af4536f4df105704a
Ward Meersman <wardmeersman@gmail.com>
2022-05-18 10:23:05 +0200

    prettier

--- frontend/src/views/LoginPage/LoginPage.test.tsx
+++ frontend/src/views/LoginPage/LoginPage.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import LoginPage from "./LoginPage";
 
@@ -6,34 +6,33 @@ import "@testing-library/jest-dom";
 
 const mockedUsedNavigate = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useNavigate: () => (mockedUsedNavigate),
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: () => mockedUsedNavigate,
 }));
 
-
-test('has an email field and it changes', () => {
+test("has an email field and it changes", () => {
     render(<LoginPage />);
     const emailField = screen.getByPlaceholderText("name@example.com");
-    expect(emailField).toHaveValue("")
+    expect(emailField).toHaveValue("");
     userEvent.type(emailField, "email@email.com");
-    expect(emailField).toHaveValue("email@email.com")
-})
+    expect(emailField).toHaveValue("email@email.com");
+});
 
-test('has an password field and it changes', () => {
+test("has an password field and it changes", () => {
     render(<LoginPage />);
     const passwordField = screen.getByPlaceholderText("Password");
-    expect(passwordField).toHaveValue("")
+    expect(passwordField).toHaveValue("");
     userEvent.type(passwordField, "wachtwoord");
-    expect(passwordField).toHaveValue("wachtwoord")
-})
+    expect(passwordField).toHaveValue("wachtwoord");
+});
 
-test('has headings', () => {
+test("has headings", () => {
     render(<LoginPage />);
-    const headings = screen.getAllByRole('heading')
-    expect(headings[0]).toHaveTextContent("Hi there!")
-    expect(headings[1]).toHaveTextContent("Welcome to the Open Summer of Code selections app.")
-    expect(headings[2]).toHaveTextContent("After you've logged in with your account, we'll enable your account so you can get started. An admin will verify you as soon as possible.")
-})
-
-
+    const headings = screen.getAllByRole("heading");
+    expect(headings[0]).toHaveTextContent("Hi there!");
+    expect(headings[1]).toHaveTextContent("Welcome to the Open Summer of Code selections app.");
+    expect(headings[2]).toHaveTextContent(
+        "After you've logged in with your account, we'll enable your account so you can get started. An admin will verify you as soon as possible."
+    );
+});
