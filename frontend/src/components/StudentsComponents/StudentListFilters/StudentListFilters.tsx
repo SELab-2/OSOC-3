56fd1055536b62cccb4886d9c486da504ea9e277
beguille <bert.guillemyn@ugent.be>
2022-05-19 09:12:48 +0200

    possible implementation for students reload

--- frontend/src/components/StudentsComponents/StudentListFilters/StudentListFilters.tsx
+++ frontend/src/components/StudentsComponents/StudentListFilters/StudentListFilters.tsx
@@ -33,7 +33,7 @@ export default function StudentListFilters() {
     /**
      * Request all students with selected filters
      */
-    async function getData(requested: number) {
+    async function getData(requested: number, edChange: boolean = false) {
         const filterChanged = requested === -1;
         const requestedPage = requested === -1 ? 0 : page;
 
@@ -41,7 +41,7 @@ export default function StudentListFilters() {
             return;
         }
 
-        if (allDataFetched) {
+        if (allDataFetched && !edChange) {
             const tempStudents = allStudents
                 .filter(student =>
                     (student.firstName + " " + student.lastName)
@@ -127,6 +127,16 @@ export default function StudentListFilters() {
         getData(-1);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [nameFilter, rolesFilter, alumniFilter, studentCoachVolunteerFilter]);
+
+    useEffect(() => {
+        setStudents([]);
+        setAllStudents([]);
+        setPage(0);
+        setAllDataFetched(false);
+        setMoreDataAvailable(true);
+        getData(-1, true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [params.editionId]);
 
     let list;
     if (students.length === 0) {
