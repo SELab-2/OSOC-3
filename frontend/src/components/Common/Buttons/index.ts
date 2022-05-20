e6f3eabb37d7e8fd1ee60fa50dc245b80faf9173
stijndcl <declercq.stijn@outlook.com>
2022-05-20 16:42:12 +0200

    Create a common orange button

--- frontend/src/components/Common/Buttons/index.ts
+++ frontend/src/components/Common/Buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as CreateButton } from "./CreateButton";
 export { default as DeleteButton } from "./DeleteButton";
+export { default as OrangeButton } from "./OrangeButton";
 export { default as WarningButton } from "./WarningButton";
