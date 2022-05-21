56dab10eba20e7f20dbe5f02cf869f1ea09e6e92
cledloof <clement.deloof@gmail.com>
2022-05-21 14:59:27 +0200

    removed white line

--- frontend/src/components/StudentsComponents/StudentList/styles.ts
+++ frontend/src/components/StudentsComponents/StudentList/styles.ts
@@ -3,6 +3,5 @@ import styled from "styled-components";
 export const StudentCardsList = styled.div`
     height: 60%;
     overflow-y: auto;
-    border-bottom: 1px solid white;
     margin-top: 2%;
 `;
